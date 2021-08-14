32489bf05ecb6c8a3e9826c4921896f419dd6d88
evdigitech <anshu.a@deductiveclouds.com>
2021-08-14 11:17:51 +0530

    ticket dm

---
+++ src/pages/booking/tickets/TicketsConfirm.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function TicketsConfirm() {
+  return (
+    <>
+      {/* <div>
+        <Container>
+          <h3>Confirm your Details</h3>
+          <div>
+            <Row>
+              <Col>
+                <span>Tirathgarh</span>
+              </Col>
+              <Col>
+                <span>Change</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <span>Car Parking x 2</span>
+              </Col>
+              <Col>
+                <span>40  </span>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <span>Trekking x 5</span>
+              </Col>
+              <Col>
+                <span> 500</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <span> Boating x 5 </span>
+              </Col>
+              <Col>
+                <span>100</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <span>Sightseeing x 6</span>
+              </Col>
+              <Col>
+                <span>240</span>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div> */}
+    </>
+  );
+}
+
+export default TicketsConfirm;
--- src/pages/dm pass/Locations.js
+++ src/pages/dm pass/Locations.js
@@ -29,9 +29,9 @@ function Locations() {
 
   return (
     <>
-      <div>
+     {/*  <div>
         <div className="dmpass-div">
-          {/* <Container className="dm-kangervilla">
+          <Container className="dm-kangervilla">
             <FaArrowLeft className="kanger-arrow" />
             <div className="kangervilla">
               <span className="kanger-valley">
@@ -41,7 +41,7 @@ function Locations() {
               </span>
             </div>
           </Container> */}
-        </div>       
+        {/* </div>       
         <Container className="dmpass-form mt-5">
           <Row className="dmpassData">
             <h3 style={{ fontWeight: "bolder", textAlign: "center" }}>
@@ -230,9 +230,108 @@ function Locations() {
               </Row>
             </div>              
          
-        </Container>  
-              
+        </Container>              
+      </div> */}
+
+
+
+
+
+
+
+      <div>
+        <Container>
+          <h3  style={{textAlign:"center"}}>Confirm your Details</h3>
+         <div className="confirm-main">
+         <div className="confirm_div" style={{textAlign:"center",backgroundColor:"#F8F8F8" ,marginBottom:"10px"}}>
+            <Row>
+              <Col xs={6} md={6}>
+                <span className="confirm-title">Tirathgarh</span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span>Change</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <span className="confirm_part">Car Parking x 2</span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">40  </span>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">Trekking x 5</span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part"> 500</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part"> Boating x 5 </span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">100</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">Sightseeing x 6</span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">240</span>
+              </Col>
+            </Row>
+          </div>
+
+         </div>
+          <div  className="confirm_div" style={{textAlign:"center",backgroundColor:"#F8F8F8",marginBottom:"10px"}}>
+            <Row>
+              <Col xs={6} md={6}>
+                <span className="confirm-title">Tamdaghumar</span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span>Change</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">Car Parking x 2</span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">40  </span>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">Trekking x 5</span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part"> 500</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part"> Boating x 5 </span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">100</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">Sightseeing x 6</span>
+              </Col>
+              <Col xs={6} md={6}>
+                <span  className="confirm_part">240</span>
+              </Col>
+            </Row>
+          </div>
+        </Container>
       </div>
+
 
     </>
   );
