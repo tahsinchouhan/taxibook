08c3f0b5eb352f440786b72f79ca956df30ac59a
evdigitech <evdigitech@gmail.com>
2021-09-01 16:03:16 +0530

    search Api working

--- src/redux/bus/saga.js
+++ src/redux/bus/saga.js
@@ -55,8 +55,7 @@ function* createBusBooking({ payload }) {
     console.log("pay",payload); 
     const phone = JSON.parse(localStorage.getItem("mobile"))  
     try {
-        const busticket = yield call(createBusBookingRequest, {...payload, mobile:phone});
-       
+        const busticket = yield call(createBusBookingRequest, {...payload, mobile:phone, email: busticket.data.email});       
         yield put(setBookingId(busticket.data.booking_Id));
         console.log("bus",busticket);
     } catch (error) {
