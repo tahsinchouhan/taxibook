eea28b5afc1af7dec4f6106c9ad1697e3454d89c
gautam180 <gautamsarawagi1@gmail.com>
2021-11-10 17:31:51 +0530

    Newely Updated code

--- src/pages/marquee.js
+++ src/pages/marquee.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useHistory} from "react-router-dom"
-
+import Logo from '.././assets/img/new_Logo.png'
 function MarqueeComponent() {
     const history = useHistory()
     const marqueeHandler = () => {
@@ -10,8 +10,8 @@ function MarqueeComponent() {
         <>
         <div className="pt-3">
         <marquee width="100%" direction="right" height="1%" style={{color:'black', cursor:'pointer'}} onClick={marqueeHandler}>
-        Book Bus Tickets 	&#11088;
-      
+        Book Bus Tickets <img src={Logo} style={{height:'35px'}}/>
+        {/* &#11088; */}
    </marquee>
         </div>
         </>
