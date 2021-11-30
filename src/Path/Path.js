f82cd5484cd934e22d47e418c7807c799a046f91
gautam180 <gautamsarawagi1@gmail.com>
2021-11-30 13:01:59 +0530

    api path changed

--- src/Path/Path.js
+++ src/Path/Path.js
@@ -1,2 +1,4 @@
-export const API_PATH = "https://www.anshuagrawal.in";
+// export const API_PATH = "https://www.anshuagrawal.in";
 //  export const API_PATH = "http://localhost:4001";
+ export const API_PATH = "http://15.206.92.158";
+
