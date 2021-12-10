b528a2e9ae7fb766422422b7c15e6a73f874163b
nbdigitech <nbdigitech>
2021-12-10 11:38:16 +0530

    url changes

--- src/Path/Path.js
+++ src/Path/Path.js
@@ -1,2 +1,2 @@
-export const API_PATH = "https://www.anshuagrawal.in";
-// export const API_PATH = "https://travelbastar.com";
+//export const API_PATH = "https://www.anshuagrawal.in";
+ export const API_PATH = "https://travelbastar.com";
