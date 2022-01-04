9b492b8d0473a24bbaf11c9648cc94fc1b862113
lata <lataadhikari1111@gmail.com>
2022-01-04 13:13:27 +0530

    changes in selectbooking

--- src/pages/selectbooking/SelectBooking.js
+++ src/pages/selectbooking/SelectBooking.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Row, Col, Container } from "react-bootstrap";
 // import logo from "../../assets/img/logo.png";
 // import city from "../../assets/img/city.png";
 // import calendar from "../../assets/img/calendar.png";
@@ -9,12 +9,28 @@ import "react-datepicker/dist/react-datepicker.css";
 // import BusPass from "./BusPass";
 import { useHistory } from "react-router-dom";
 import doodle from "../../assets/img/doodle.png";
-import bus from "../../assets/img/bus.png";
 import cab from "../../assets/img/cab.png";
-import hotel from "../../assets/img/hotel.png";
-import ticket from "../../assets/img/ticket.png";
 import Header from "../../components/Header";
 import Footer from "../travesaly/Footer";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import CarouselReact from "react-multi-carousel";
+import Carousel from "react-bootstrap/Carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Image } from "react-bootstrap";
+
+
+import TravelPass from "../../assets/img/TravelPass.jpg";
+import Ticket from "../../assets/img/Ticket.jpg";
+import HomeStay from "../../assets/img/HomeStay.jpg";
+import Bus from "../../assets/img/Bus.jpg";
+import TourPackage from "../../assets/img/TourPackage.jpg";
+import Taxi from "../../assets/img/Taxi.jpg";
+import TourGuid from "../../assets/img/TourGuid.jpeg";
+import TourOperator from "../../assets/img/TourOperator.jpg";
+import Event from "../../assets/img/Event.jpg";
+import Hotel from "../../assets/img/Hotel.jpg";
+
 
 function SelectBooking() {
   // const [passTab, setPassTab] = useState(false);
@@ -22,6 +38,7 @@ function SelectBooking() {
   // const [startDate, setStartDate] = useState(new Date());
   // const [busjourney, setBusJourney] = useState(false);
   // const [cabjourney, setCabJourney] = useState(false);
+  const [carouselIndex, setCarouselIndex] = useState(0);
 
   const history = useHistory();
 
@@ -49,114 +66,206 @@ function SelectBooking() {
   //   history.push("/tickets");
   // };
 
-  const clickEventHandler = href => {
+  const clickEventHandler = (href) => {
     history.push(href);
-  }
+  };
 
   const list = [
     {
-      img:bus,
+      img: Bus,
       title: 'Bus',
       desc: 'Book bus ticket for your favourite spots',
       link: '/buspass'
     },
     {
-      img:hotel,
+      img: Hotel,
       title: 'Hotel',
       desc: 'Hotels for your favourite spots',
       link: '/hotellist'
     },
     {
-      img:doodle,
+      img: Ticket,
       title: 'Ticket',
       desc: 'Registered ticket services for a personal travel experience',
       link: '/tickets'
     },
     {
-      img:cab,
+      img: Taxi,
       title: 'Taxi',
       desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       link: '/taxiinbastar'
     },
     {
-      img:ticket,
+      img: TravelPass,
       title: 'Travel Pass',
       desc: 'Registered ticket services for a personal travel experience',
       link: '/dmpass'
     },
     {
-      img:ticket,
+      img: HomeStay,
       title: 'Home Stay',
       desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       link: '/homestaysinbastar'
     },
     {
-      img:ticket,
+      img: TourPackage,
       title: 'Tour Package',
       desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       link: '/tourpackagesinbastar'
     },
-    
+
     {
-      img:ticket,
+      img: TourGuid,
       title: 'Tour Guide',
       desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       link: '/tourguidesinbastar'
     },
     {
-      img:ticket,
+      img: TourOperator,
       title: 'Tour Operators',
       desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       link: '/touroperatorsinbastar'
     },
     {
-      img:ticket,
+      img: Event,
       title: 'Event & Activity',
       desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
       link: '/bastareventsandactivities'
-    }
-  ]
+    },
+  ];
+
+  const Para = {
+    fontSize: "22px",
+    fontStyle: "italic",
+    fontFamily: '"Josefin Sans", sans-serif',
+    marginTop: "-30px",
+
+  };
+
+  const card = {
+    marginTop: "-140px",
+    fontWeight: "bold",
+    color: "DarkRed",
+    backgroundColor: "#e9ecef",
+    width: "150px",
+    marginLeft: "-16px",
+    borderRadius: "5px"
+  }
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 7,
+      slidesToSlide: 4,
+
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+      slidesToSlide: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+      slidesToSlide: 1,
+    },
+  };
 
   return (
     <>
-      <Header />
-      <div className="d-none d-md-block">
-        <div className="select_div">
-          <div className="col-xs-12  col-sm-12 col-md-12">
-            <div
-              className="booking-div py-5 mt-5"
-              style={{ textAlign: "center" }}
-            >
-              <span style={{ fontWeight: "bolder", fontSize: "20px" }}>
-                What do you wish to book?
-              </span>
-            </div>
-          </div>
+      <div>
+        <Header />
+        <div>
+
+          <Carousel
+            activeIndex={carouselIndex}
+          >
+            {list.map((data, idx) => {
+              const { img, title, desc, link } = data;
+              return (
+                <Carousel.Item>
+                  <div>
+                    <img
+                      style={{
+                        height: "500px",
+                        width: "100%",
+
+                        filter: "brightness(0.5)"
+                      }}
+                      src={img}
+                      alt="Image"
+                    />
+                  </div>
+                  <Carousel.Caption>
+                    <div
+                      style={{ marginTop: "-450px", textAlign: "left" }}>
+
+                      <h3 style={{
+                        fontWeight: "bold",
+                        fontSize: "40px",
+                        color: "DarkRed",
+                        marginTop: "100px"
+                      }}
+                      >
+                        {title}
+                      </h3>
+                      <p style={Para}> {desc} </p>
+                      <Button onClick={() => clickEventHandler(link)} style={{ marginTop: "-30px" }}>
+                        Book Now
+                      </Button>
+
+                    </div>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
         </div>
-        <Container style={{width:"971px"}}>
-            <Row>
-              {
-                list.map((data,idx) => {
-                  const { img, title, desc, link } = data
-                  return (
-                    <Col key={idx} xs={12} md={4}>
-                      <div className="userdatas" onClick={()=>clickEventHandler(link)}>
-                        <div><img src={img} alt={img} style={{ width: 39, height: 25.58, marginTop: "10px" }} /></div>
-                          <div className="ps-3">
-                            <p className="booking_icon my-0 text-secondary">{title}</p>
-                            <p>{desc}</p>
+
+        <Container style={{ position: "relative", zIndex: "4" }}>
+          <div style={{ marginTop: "-80px" }}>
+            {list.length > 0 ? (
+              <CarouselReact
+                ssr
+                partialVisible
+                itemClass="image-item"
+                responsive={responsive}
+              >
+                {list.length > 0
+                  ? list.map((data, idx) => {
+                    const { img, title, desc, link } = data;
+                    return (
+                      <div onClick={(e) => { setCarouselIndex(idx) }} >
+                        <div style={{ marginLeft: "10px" }} >
+                          <Image
+                            draggable={false}
+                            style={{ width: "150px", height: "180px", borderRadius: "10px" }}
+                            src={img}
+                          />
+                        </div>
+                        <Carousel.Caption>
+                          <div>
+                            <h6 className="packages_block-title_mt-3 mb-0" style={card}>
+                              {title}
+                            </h6>
                           </div>
+                        </Carousel.Caption>
                       </div>
-                    </Col>
-                  )
-                })
-              }
-            </Row>
+                    );
+                  })
+                  : null}
+              </CarouselReact>
+            ) : null}
+          </div>
+
+
         </Container>
-        <div className="pt-5 mt-5">
-          <Footer />
-        </div>
+
       </div>
+
+      <div className="pt-5 mt-5">
+        <Footer />
+      </div>
+
 
       {/*mobile*/}
       <div className="d-block d-md-none">
@@ -172,24 +281,24 @@ function SelectBooking() {
           </div>
 
           <Container>
-              <Row>
+            <Row>
               {
-                list.map((data,idx) => {
+                list.map((data, idx) => {
                   const { img, title, desc, link } = data
                   return (
                     <Col key={idx} xs={12} md={4}>
-                      <div className="userdatas" onClick={()=>clickEventHandler(link)}>
-                        <div><img src={img} alt ={img} style={{ width: 39, height: 25.58, marginTop: "10px" }} /></div>
-                          <div className="ps-3">
-                            <p className="booking_icon my-0 text-secondary">{title}</p>
-                            <p>{desc}</p>
-                          </div>
+                      <div className="userdatas" onClick={() => clickEventHandler(link)}>
+                        <div><img src={img} alt={img} style={{ width: 39, height: 25.58, marginTop: "10px" }} /></div>
+                        <div className="ps-3">
+                          <p className="booking_icon my-0 text-secondary">{title}</p>
+                          <p>{desc}</p>
+                        </div>
                       </div>
                     </Col>
                   )
                 })
               }
-              </Row>
+            </Row>
           </Container>
         </div>
       </div>
